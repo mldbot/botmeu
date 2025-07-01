40a2cbc5891ee76b1525e0b68309d02600a8adfd
mldbot <chillzonemrp@gmail.com>
2025-07-02 00:12:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
 import os
+import random
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from twilio.rest import Client
 
+# Verificăm dacă variabilele de mediu sunt setate corect
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SERVER_ID = int(os.getenv("SERVER_ID"))
 TWILIO_SID = os.getenv("TWILIO_SID")
 TWILIO_TOKEN = os.getenv("TWILIO_TOKEN")
 TWILIO_NUMBER = os.getenv("TWILIO_NUMBER")
 NGROK_URL = os.getenv("NGROK_URL")
+
+if not all([BOT_TOKEN, SERVER_ID, TWILIO_SID, TWILIO_TOKEN, TWILIO_NUMBER, NGROK_URL]):
+    raise EnvironmentError("Unele variabile de mediu sunt lipsite!")
 
 bot = commands.Bot(command_prefix="!")
 slash = SlashCommand(bot, sync_commands=True)
@@ -27,7 +32,7 @@ twilio_client = Client(TWILIO_SID, TWILIO_TOKEN)
 )
 async def _dial(ctx: SlashContext, cell_phone: str, otp_digits: int, client_name: str, company_name: str):
     await ctx.send(f"\U0001F4DE Trimit apel către `{cell_phone}` cu OTP de {otp_digits} cifre...")
-    otp = "".join(str(__import__("random").randint(0, 9)) for _ in range(otp_digits))
+    otp = "".join(str(random.randint(0, 9)) for _ in range(otp_digits))  # Folosind random direct
     call = twilio_client.calls.create(
         to=cell_phone,
         from_=TWILIO_NUMBER,
